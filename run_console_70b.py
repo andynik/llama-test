deecd4d4605e088612e4f6434ef53a317144f5f3
Andrii Nikolaiev <nikolaev@knu.ua>
2023-12-20 16:57:41 +0000

    Add files via upload

---
+++ run_console_70b.py
@@ -0,0 +1,44 @@
+#!/bin/python
+import os
+import sys
+from termcolor import colored
+from llama_cpp import Llama
+
+# MODEL_PATH = "wizardLM-7B-GGML/wizardLM-7B.ggmlv3.q4_1.bin"
+# os.add_dll_directory() # for resolving .dll error
+# MODEL_PATH = "Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q4_0.gguf"
+MODEL_PATH = "Llama-2-70B-Chat-GGUF/llama-2-70b.Q4_K_M.gguf"
+
+MAX_TOKENS = 2048
+
+def run_console_app():
+    if not os.path.isfile(MODEL_PATH):
+        print(f"Error: Invalid model path '{MODEL_PATH}'")
+        sys.exit(1)
+
+    llm = Llama(model_path=MODEL_PATH)
+
+    print(colored("\n## Welcome to the Llama Console App! ##\n", "yellow"))
+    print("Enter your prompt (or 'q' to quit):")
+
+    while True:
+        user_input = input("> ")
+        if user_input.lower() == 'q':
+            print("Exiting...")
+            break
+
+        user_input = f"Q: {user_input} A:"
+        print(colored("Processing...", "yellow"))
+
+        try:
+            output = llm(user_input, max_tokens=MAX_TOKENS, stop=["Q:"], echo=True)
+
+            choices_text = output["choices"][0]["text"]
+            choices_text = choices_text.replace(user_input, "").strip()
+            formatted_text = colored(choices_text, "green")
+            print(f"\n{formatted_text}\n")
+        except Exception as e:
+            print(f"Error: Failed to generate response. {str(e)}")
+
+if __name__ == '__main__':
+    run_console_app()
